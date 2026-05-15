bf4ba02051b76de4a9dcafd3ebcec9b4818892ee
Kristiyan Ivanov <kivanov@hilscher.com>
2026-05-15 13:44:13 +0000

    push to main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KristiyanIvanow/go-test-app.git
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/KristiyanIvanow/go-test-app.git/src/greeting"
+
+
+func main() {
+	greeting.Greet()
+}
